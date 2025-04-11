2287998e4b25d71035a24418a1405f9dec7c3c75
Samuel Muniz <137716288+DevSamuelMuniz@users.noreply.github.com>
2025-04-11 17:51:15 -0300

    Add files via upload

---
+++ get_dataset.py
@@ -0,0 +1,9 @@
+from pysus import SINAN
+
+# sinan = SINAN().load()
+
+# files_sinan = sinan.get_files(dis_code=["CANC"])
+
+# files_sinan1 = sinan.get_files(dis_code="CANC", year=[2024, 2023])
+
+# sinan.download(files_sinan1)
